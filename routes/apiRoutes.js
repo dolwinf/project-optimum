ce8a1c56a4dd5e525d7c15d1322fc6812e5ebfec
Dolwin Fernandes <dolwinf@gmail.com>
2019-11-07 16:54:43 +1100

    Added login logic part1

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,61 @@
 var db = require("../models");
+const errors = [];
 
 module.exports = function(app) {
-	// Get all examples
-	app.get("/api/examples", function(req, res) {
-		// db.Example.findAll({}).then(function(dbExamples) {
-		//   res.json(dbExamples);
-		// });
-	});
+  // Get all examples
+  app.get("/api/examples", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.json(dbExamples);
+    // });
+  });
 
-	// Create a new example
-	app.post("/api/users", function(req, res) {
-		db.user.create(req.body).then(function(data) {
-			res.redirect("/success");
-		});
-	});
+  app.post("/api/users/login", function(req, res) {
+    db.user
+      .findAll({
+        where: {
+          email: req.body.email
+        }
+      })
+      .then(function(data) {
+        console.log(data);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  });
 
-	// Delete an example by id
-	app.delete("/api/examples/:id", function(req, res) {
-		// db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-		//   res.json(dbExample);
-		// });
-	});
+  // Create a new user
+  app.post("/api/users", function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordr = req.body.passwordr;
+
+    if (!email || !password || !passwordr) {
+      errors.push({ msg: "Please enter all fields." });
+    }
+    if (password !== passwordr) {
+      errors.push({
+        msg: "Password do not match. Please enter password again."
+      });
+    }
+
+    if (password.length < 9) {
+      errors.push({ msg: "Password should be more than 8 characters." });
+    }
+
+    if (errors.length > 0) {
+      res.redirect("/register");
+    } else {
+      db.user.create(req.body).then(function(data) {
+        res.redirect("/");
+      });
+    }
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function(req, res) {
+    // db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+    //   res.json(dbExample);
+    // });
+  });
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,15 +2,24 @@ var db = require("../models");
 var path = require("path");
 
 module.exports = function(app) {
-  // Load index page
-  app.get("/", function(req, res) {
+  app.get("/register", function(req, res) {
     // db.Example.findAll({}).then(function(dbExamples) {
     //   res.render("index", {
     //     msg: "Welcome!",
     //     examples: dbExamples
     //   });
     // });
-    res.sendFile(path.join(__dirname, "../public/register.html"));
+    res.sendFile(path.join(__dirname, "../public/views/register.html"));
+  });
+
+  app.get("/login", function(req, res) {
+    // db.Example.findAll({}).then(function(dbExamples) {
+    //   res.render("index", {
+    //     msg: "Welcome!",
+    //     examples: dbExamples
+    //   });
+    // });
+    res.sendFile(path.join(__dirname, "../public/views/login.html"));
   });
 
   // Load example page and pass in an example by id
@@ -24,6 +33,9 @@ module.exports = function(app) {
     // });
   });
 
+  app.get("/success", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/views/thanks.html"));
+  });
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.send("404");
