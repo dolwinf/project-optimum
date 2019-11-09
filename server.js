cd5b4bd317d9af641951d8e52cbd766a3021fe04
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-11-09 12:03:05 +1100

    removed config from folder merge to model

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
-
 var db = require("./models");
-
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -34,14 +31,13 @@ if (process.env.NODE_ENV === "test") {
 }
 
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
+db.sequelize.sync().then(function () {
+  app.listen(PORT, function () {
+    console.log("Listening on port", PORT);
   });
 });
+
+
+
 
 module.exports = app;
