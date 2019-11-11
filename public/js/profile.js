138194709a16dd0e05c240bb8b4096e11bd94be8
Alex Wan <a_wanderous@hotmail.com>
2019-11-11 23:18:39 +1100

    added profile expand and add item expand

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,8 +1,20 @@
-$(document).on("click", "#add-item", function () {
-    $("html, body").animate({
-        scrollTop: $("#add-item").offset().top
-    }, 1000)
+$("#profile").click(function () {
+    $("#profile-form").show(2000);
 });
 
+$("#profile").blur(function () {
+    $("#profile-form").hide(2000);
+    $("#profile-form")[0].reset();
+});
+
+$("#add-item").click(function () {
+    $("#add-form").show(2000);
+});
+
+$("#add-item").focusout(function () {
+    $("#add-form").hide(2000);
+    $("#add-form")[0].reset();
+});
 // TODO delete after data insert
-$("#username").text("N00bKiller69");
+$(".username").text("N00bKiller69");
+
