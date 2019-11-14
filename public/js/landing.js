911de5cce5e03f3cfff02d7a6e8692c81e101cb1
Dolwin Fernandes <dolwinf@gmail.com>
2019-11-14 12:59:13 +1100

    HTML links and slight color changes on card body

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,20 +1,19 @@
-$(".logout").on("click", function (e) {
+$(".logout").on("click", function(e) {
   e.preventDefault();
   $.ajax({
     method: "GET",
     url: "/logout"
-  }).then(function () {
+  }).then(function() {
     window.location.href = "/";
   });
 });
 
-window.onload = function () {
+window.onload = function() {
   $.ajax({
     method: "GET",
     url: "/getdata"
-  }).then(function (data) {
-    data.forEach(function (item) {
-
+  }).then(function(data) {
+    data.forEach(function(item) {
       var firstName = "Tom";
       var lastName = "Smith";
       var mobile = "0468 555 123";
@@ -24,30 +23,40 @@ window.onload = function () {
       var result =
         "<div class='card' style='flex-direction: row;margin: 0.5rem;'><img src=" +
         item.ImageURL +
-        " class='card-img-top' alt='...'><div class='card-body'><h5 class='card-title'>" +
+        " class='card-img-top' alt='...'><div class='card-body' style='background-color:#ffce6b'><h5 class='card-title'>" +
         item.Name +
         "</h5><p class='card-text'>" +
         item.Description +
         "</p><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>Swapz It!</button></div></div>" +
-        "<div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' id='exampleModalLabel'>" + item.Name + "</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'> Exchanger details:</br>" +
-        "Name: " + firstName + " " + lastName + "</br>" +
-        "Mobile: " + mobile + "</br>" +
-        "Email: " + email + "</br>" +
-        "Home Address: " + homeAddress
-        +
-        "</div><div class='modal-footer'><button type='button' class='btn btn-secondary' data-dismiss='modal'>Close</button>";
+        "<div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' id='exampleModalLabel'>" +
+        item.Name +
+        "</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'> Exchanger details:</br>" +
+        "Name: " +
+        firstName +
+        " " +
+        lastName +
+        "</br>" +
+        "Mobile: " +
+        mobile +
+        "</br>" +
+        "Email: " +
+        email +
+        "</br>" +
+        "Home Address: " +
+        homeAddress +
+        "</div><div class='modal-footer'><button type='button' class='btn btn-info' data-dismiss='modal'>Close</button>";
 
       $("#items").prepend(result);
     });
   });
 };
 
-$(".editProfile").on("click", function (e) {
+$(".editProfile").on("click", function(e) {
   e.preventDefault();
   window.location.href = "/profile";
 });
 
-$(".addItem").on("click", function (e) {
+$(".addItem").on("click", function(e) {
   e.preventDefault();
   window.location.href = "/addItem";
 });
