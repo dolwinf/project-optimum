e4bd033d8a738e37bb409c9f7e65a108215bb7e5
Hugo <52917408+hugo-engineer@users.noreply.github.com>
2019-11-14 18:26:15 +1100

    button fix

--- public/js/landing.js
+++ public/js/landing.js
@@ -15,6 +15,8 @@ window.onload = function () {
     url: "/getdata"
   }).then(function (data) {
     data.forEach(function (item) {
+
+      console.log(item)
       var firstName = "Tom";
       var lastName = "Smith";
       var mobile = "0468 555 123";
@@ -28,8 +30,8 @@ window.onload = function () {
         item.Name +
         "</h5><p class='card-text'>" +
         item.Description +
-        "</p><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal'>Swapz It!</button></div></div>" +
-        "<div class='modal fade' id='exampleModal' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' id='exampleModalLabel'>" +
+        "</p><button type='button' class='btn btn-primary' data-toggle='modal' data-target='#exampleModal" + item.id + "'>Swapz It!</button></div></div>" +
+        "<div class='modal fade' id='exampleModal" + item.id + "' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><h5 class='modal-title' id='exampleModalLabel'>" +
         item.user.firstName +
         "</h5><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div><div class='modal-body'> Exchanger details:</br>" +
         "Name: " +
@@ -61,3 +63,5 @@ $(".addItem").on("click", function (e) {
   e.preventDefault();
   window.location.href = "/addItem";
 });
+
+
